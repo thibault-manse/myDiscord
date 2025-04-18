5a363ba6389364be75303857cd8b2a9040200d84
thibault-manse <thibault.manse@laplateforme.io>
2025-04-18 11:29:35 +0200

    update : Banned member add

--- myDiscord2.sql
+++ myDiscord2.sql
@@ -25,31 +25,24 @@ CREATE TABLE utilisateurs (
 	image_url TEXT NOT NULL
 );
 
-INSERT INTO utilisateurs VALUES (1,'Takemi13', 'Biduledu13@gmail.com', '12345', 'Je sais pas quoi mettre', 'Mon image'),
-(2, 'Ruirui@Pilaf', 'ZoumTallon@gmail.com', '12345', 'Je vend du viagra à 6 centimes', 'Mon image'),
-(3, 'Mochi', 'Zebullon82@gmail.com', '1234', 'NI KONI KONIIIIIII :3', 'Mon image');
-
 DROP TABLE IF EXISTS serveur CASCADE;
 
 CREATE TABLE serveur(
 	serveur_id SERIAL PRIMARY KEY,
 	nom VARCHAR(50) NOT NULL,
 	createur_id INT REFERENCES utilisateurs (utilisateur_id),
-	privé INT NOT NULL,
+	privé INT NOT NULL, --0 = Publique; 1 = Privé
 	image_url TEXT NOT NULL
 );
-
-INSERT INTO serveur VALUES (1,'Premier serveur', 1, 0, 'Une image'), (2, 'tchat privé', 1, 1, 'une image');
 
 DROP TABLE IF EXISTS membre_serveur CASCADE;
 
 CREATE TABLE membre_serveur(
 	utilisateur_id INT REFERENCES utilisateurs (utilisateur_id),
 	serveur_id INT REFERENCES serveur (serveur_id),
+	banni INT NOT NULL, --0 = Non banni; 1 = Banni
 	PRIMARY KEY (utilisateur_id, serveur_id)
 );
-
-INSERT INTO membre_serveur VALUES (1, 1), (1, 2), (2, 1), (2, 2), (3,1);
 
 DROP TABLE IF EXISTS tchat CASCADE;
 
@@ -58,8 +51,6 @@ CREATE TABLE tchat (
 	nom VARCHAR(50) NOT NULL,
 	serveur_id INT REFERENCES serveur(serveur_id)
 );
-
-INSERT INTO tchat VALUES (1, 'general', 1), (2, 'connerie', 1), (3, 'privé', 2);
 
 DROP TABLE IF EXISTS messages CASCADE;
 
@@ -101,3 +92,14 @@ CREATE TABLE fichier(
 	url_fichier TEXT NOT NULL,
 	type_fichier VARCHAR(255) NOT NULL
 );
+
+
+INSERT INTO utilisateurs VALUES (1,'Takemi13', 'Biduledu13@gmail.com', '12345', 'Je sais pas quoi mettre', 'Mon image'),
+(2, 'Ruirui@Pilaf', 'ZoumTallon@gmail.com', '12345', 'Je vend du viagra à 6 centimes', 'Mon image'),
+(3, 'Mochi', 'Zebullon82@gmail.com', '1234', 'NI KONI KONIIIIIII :3', 'Mon image');
+
+INSERT INTO serveur VALUES (1,'Premier serveur', 1, 0, 'Une image'), (2, 'tchat privé', 1, 1, 'une image');
+
+INSERT INTO membre_serveur VALUES (1, 1, 0), (1, 2, 0), (2, 1, 0), (2, 2, 0), (3,1, 0);
+
+INSERT INTO tchat VALUES (1, 'general', 1), (2, 'connerie', 1), (3, 'privé', 2);
