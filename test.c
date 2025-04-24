025c3d3e66b98bc5d699958f324b2a06f7046140
thibault-manse <thibault.manse@laplateforme.io>
2025-04-24 14:37:20 +0200

    update : can create account

--- test.c
+++ test.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <libpq-fe.h>
 
+int choice = 0;
+
 int main() {
     // Chaîne de connexion
     const char *conninfo = "dbname=mydiscord user=postgres password=dvjmfr5e host=localhost port=5432";
@@ -19,47 +21,98 @@ int main() {
 
     printf("Connexion réussie à PostgreSQL !\n");
 
-    // Exécution d'une requête
-    char pseudo[50];
-    char motdepasse[50];
-    printf("Rentré votre pseudo : ");
-    scanf("%s", pseudo);
+    printf("Bienvenur sur myDiscord ! Voulez vous vous connecté ? (1) ou crée un compte ? (2) : ");
+    scanf("%d", &choice);
 
-    printf("Rentré votre mot de passe : ");
-    scanf("%s", motdepasse);
+    if (choice == 1){
+        // Exécution d'une requête
+        char pseudo[50];
+        char password[50];
+        printf("Rentré votre pseudo : ");
+        scanf("%s", pseudo);
 
-    const char *paramValues[2] = {pseudo, motdepasse};
+        printf("Rentré votre mot de passe : ");
+        scanf("%s", password);
 
-    PGresult *res = PQexecParams(conn,
-        "SELECT * FROM utilisateurs WHERE pseudo = $1 AND motdepasse = $2",
-        2,       // nombre de paramètres
-        NULL,    // types des paramètres (NULL = auto)
-        paramValues,
-        NULL,    // tailles (NULL = texte)
-        NULL,    // formats (NULL = texte)
-        0);      // résultat en texte (0), ou en binaire (1)
+        const char *paramValues[2] = {pseudo, password};
 
-    if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-        fprintf(stderr, "Erreur dans la requête : %s", PQerrorMessage(conn));
+        PGresult *res = PQexecParams(conn,
+            "SELECT * FROM utilisateurs WHERE pseudo = $1 AND motdepasse = $2",
+            2,       // nombre de paramètres
+            NULL,    // types des paramètres (NULL = auto)
+            paramValues,
+            NULL,    // tailles (NULL = texte)
+            NULL,    // formats (NULL = texte)
+            0);      // résultat en texte (0), ou en binaire (1)
+
+        if (PQresultStatus(res) != PGRES_TUPLES_OK) {
+            fprintf(stderr, "Erreur dans la requête : %s", PQerrorMessage(conn));
+            PQclear(res);
+            PQfinish(conn);
+            return 1;
+        }
+
+        // Lecture des résultats
+        int nrows = PQntuples(res);
+        int ncols = PQnfields(res);
+
+        for (int i = 0; i < nrows; i++) {
+            for (int j = 0; j < ncols; j++) {
+                printf("%s\t", PQgetvalue(res, i, j));
+            }
+            printf("\n");
+        }
+
+        // Libération et fermeture
         PQclear(res);
         PQfinish(conn);
-        return 1;
+
+        return 0;
     }
 
-    // Lecture des résultats
-    int nrows = PQntuples(res);
-    int ncols = PQnfields(res);
+    else if (choice == 2){
+        char new_pseudo[50];
+        char new_email[255];
+        char new_password[50];
+        //char check_password[50];
+        //int i = 0;
+        printf("Rentré votre pseudo : ");
+        scanf("%s", new_pseudo);
 
-    for (int i = 0; i < nrows; i++) {
-        for (int j = 0; j < ncols; j++) {
-            printf("%s\t", PQgetvalue(res, i, j));
-        }
-        printf("\n");
+        printf("Rentré votre email : ");
+        scanf("%s", new_email);
+
+        printf("Rentré votre mot de passe : ");
+        scanf("%s", new_password);
+
+        const char *paramValues[3] = {new_pseudo, new_email, new_password};
+
+        PGresult *res = PQexecParams(conn,
+            "INSERT INTO utilisateurs (pseudo, mail, motdepasse, status_utilisateur, image_url) VALUES ($1, $2, $3, 2, \'mon image\')",
+            3,       // nombre de paramètres
+            NULL,    // types des paramètres (NULL = auto)
+            paramValues,
+            NULL,    // tailles (NULL = texte)
+            NULL,    // formats (NULL = texte)
+            0);      // résultat en texte (0), ou en binaire (1)
+
+        printf("Bienvenu à toi \n");
+
+        // Libération et fermeture
+        PQclear(res);
+        PQfinish(conn);
+
+        return 0;
     }
-
-    // Libération et fermeture
-    PQclear(res);
-    PQfinish(conn);
-
-    return 0;
 }
+
+
+//Début de vérification de mot de passe durant la création d'un compte
+//while (i == 0){
+//    printf("Confirmer votre mot de passe : ");
+//    scanf("%s", check_password);
+//    if (&check_password == &new_password)
+//        i++;
+//    else
+//        printf("Ce champ doit etre similaire à votre mot de passe \n");
+//}
